c8e78a4df6bcd7de6ee84a39a7ca1c01d2c38ceb
Jonas Suhr Christensen <jonas.christensen@prevas.com>
2016-09-15 15:46:50 +0200

    bake.py: add warning if building on unsupported os
    Print a warning if building on unsupported os. The warning triggers a
    prompt asking to continue or abort build. Use '-s' (--skip-os-check) option to
    automatically continue build.
    
    To add a supported OS see core/setup directory. Files can also be added in
    a setup folder in local meta layer. To unsupport an OS add a
    setup file with only the distro-release line (keep only the first line).
    
    It would be handy to have a feature to disable all other setup folders
    but the one in local layer. But so far this has not been implemented.

--- lib/oelite/cmd/bake.py
+++ lib/oelite/cmd/bake.py
@@ -1,7 +1,9 @@
 import oebakery
 import oelite.baker
 import logging
-
+import setup
+import os
+import sys
 
 description = "Build something"
 
@@ -14,6 +16,9 @@ def add_parser_options(parser):
     parser.add_option("--debug-loglines",
                       action="store", default=42, metavar="N",
                       help="Show last N lines of logfiles of failed tasks (default: 42)")
+    parser.add_option("-s", "--skip-os-check",
+                      action="store_true", default=False, metavar="skip_os_check",
+                      help="Skip supported OS check")
     return
 
 
@@ -23,8 +28,46 @@ def parse_args(options, args):
     else:
         logging.getLogger().setLevel(logging.INFO)
 
+def os_check(config):
+    distro, release = setup.get_host_distro()
+    if distro and release:
+        setup_file, supported = setup.get_setup_file(config, distro, release)
+        if supported:
+            return True
+
+    if distro and release:
+        print('WARNING: Host distribution (%s %s) has not been validated'%(distro, release))
+    else:
+        print("WARNING: Cannot determine host distribution")
+
+    response = 'n'
+
+    if os.isatty(sys.stdin.fileno()):
+        while True:
+            try:
+                response = raw_input(
+                    "Do you want to continue [Y/n]? ")
+            except KeyboardInterrupt:
+                print
+                response = 'n'
+            response = response.lower()
+            if response == '':
+                response = 'y'
+            if response in ('y', 'n'):
+                print
+                break
+
+    if response == 'n':
+        print "Maybe another time"
+        return False
+    return True
 
 def run(options, args, config):
+
+    if not options.skip_os_check:
+        if not os_check(config):
+            return 0
+
     try:
         baker = oelite.baker.OEliteBaker(options, args, config)
     except oelite.parse.ParseError as e:
